9bfe775c4ca0fad9a66eaba91cafac51d5c91a75
Ethan Hill <ethan.hill1999@outlook.com>
2025-09-24 11:25:15 -0500

    tweaks

--- setup.sh
+++ setup.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-git pull --recurse-submodules
+# Check if running on Ubuntu or Debian
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    if [[ "$ID" != "ubuntu" && "$ID" != "debian" ]]; then
+        echo "This script only runs on Ubuntu or Debian."
+        exit 1
+    fi
+else
+    echo "Cannot determine OS distribution."
+    exit 1
+fi
+
+# Check if devkitpro is already installed
+if [ -d "/opt/devkitpro" ]; then
+    echo "devkitpro is already installed."
+    exit 0
+fi
+
+sudo apt update -y
+sudo apt install -y python3 python3-is-python
+
+wget https://apt.devkitpro.org/install-devkitpro-pacman
+sudo chmod +x ./install-devkitpro-pacman
+sudo ./install-devkitpro-pacman
+sudo dkp-pacman -S gba-dev
+
+export DEVKITPRO=/opt/devkitpro
+export DEVKITARM=${DEVKITPRO}/devkitARM
+export DEVKITPPC=${DEVKITPRO}/devkitPPC 
+export PATH=${DEVKITPRO}/tools/bin:$PATH
