5d6649bcd82ae78dd4931997174d31819e622647
Ethan Hill <ethan.hill1999@outlook.com>
2025-10-06 02:10:55 -0500

    so many changes galore

---
+++ bgs/tilesets/room03.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="room03" tilewidth="32" tileheight="32" tilecount="64" columns="1">
+ <image source="room03.png" width="32" height="2048"/>
+</tileset>
